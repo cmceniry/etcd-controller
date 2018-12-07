732078afedcd9a6059b7aff0564bc26213b85d8e
Chris McEniry <cmceniry@mit.edu>
2018-12-06 16:58:35 -0800

    Fixing typo - extra command to run

--- tests/004-stop/run.sh
+++ tests/004-stop/run.sh
@@ -23,7 +23,7 @@ ctl_command /etcd-controller-ctl ${TESTNET}.101:4270 status
 echo SUCCESS
 echo "---------- ATTEMPT SECOND STOP - CHECK THAT IT FAILS ----------"
 
-ctl_command_result ctl /etcd-controller-ctl ${TESTNET}.101:4270 stop
+ctl_command_result /etcd-controller-ctl ${TESTNET}.101:4270 stop
 rc=$?
 if [ $rc -eq 0 ]; then fail -1 "expected failure. got $rc"; fi
 
